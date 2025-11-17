8dd98cac92f5264dc1c6051358ff742385345854
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-11-17 12:26:03 +0530

    Updated Contacts

--- src/components/Design/Social/index.js
+++ src/components/Design/Social/index.js
@@ -1,30 +1,26 @@
 "use client"
 import React from "react"
-import { BorderBeam } from "@/components/ui/border-beam"
-import { Card } from "@/components/ui/card"
 
 // Icons
 import { IoMailOutline } from "react-icons/io5";
 import { IoMdCall } from "react-icons/io";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
-import { AiOutlineLinkedin } from "react-icons/ai";
-import { AiOutlineFacebook } from "react-icons/ai";
 
 export function Social() {
     return (
         <ul className="flex flex-row justify-center w-full gap-2 mt-1">
             <li className="w-fit bg-[#ea4335] hover:bg-red-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105">
-                <a href="#" target="_blank"><IoMailOutline size={24} className="text-white" /></a>
+                <a href="mailto:ceo@shoolin.co.uk" target="_blank"><IoMailOutline size={24} className="text-white" /></a>
             </li>
             <li className="w-fit bg-blue-600 hover:bg-blue-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105">
-                <a href="#" target="_blank"><IoMdCall size={24} className="text-white" /></a>
+                <a href="tel:+918569856987" target="_blank"><IoMdCall size={24} className="text-white" /></a>
             </li>
             <li className="w-fit bg-green-500 hover:bg-green-600 rounded-full p-1.5 transition-all duration-300 hover:scale-105">
-                <a href="#" target="_blank"><FaWhatsapp size={24} className="text-white" /></a>
+                <a href="https://wa.me/9100000000" target="_blank"><FaWhatsapp size={24} className="text-white" /></a>
             </li>
             <li className="w-fit bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105">
-                <a href="#" target="_blank"><FaInstagram size={24} className="text-white" /></a>
+                <a href="https://instagram.com/sargampandey_" target="_blank"><FaInstagram size={24} className="text-white" /></a>
             </li>
         </ul>
     )
