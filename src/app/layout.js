a4849e68ed7ab3315ef65e49b7ff47c222e539c6
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-12-09 11:26:49 +0530

    Updated Font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import MaintenancePage from "@/components/ui/Maintenance";
 import ClientProtection from "@/components/ClientProtection";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Sargam Pandey",
@@ -11,12 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link href="https://fonts.cdnfonts.com/css/trebuchet-ms-2" rel="stylesheet" />
+      </head>
       <body className="antialiased" suppressHydrationWarning>
-        <ClientProtection>
-          {children}
-          <SpeedInsights />
-          {/* <MaintenancePage/> */}
-        </ClientProtection>
+        {/* <ClientProtection> */}
+        {children}
+        <SpeedInsights />
+        {/* <MaintenancePage/> */}
+        {/* </ClientProtection> */}
       </body>
     </html>
   );
