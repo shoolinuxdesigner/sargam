4230e5c6966db4501b1801357d0c347dce451d22
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-12-23 10:03:18 +0530

    Updated Social

--- src/components/Design/Social/index.js
+++ src/components/Design/Social/index.js
@@ -2,25 +2,23 @@
 import React from "react"
 
 // Icons
-import { IoMailOutline } from "react-icons/io5";
 import { IoMdCall } from "react-icons/io";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export function Social() {
     return (
         <ul className="flex flex-row justify-center w-full gap-2 mt-1 absolute -bottom-4.5">
-            <li className="w-fit bg-[#ea4335] hover:bg-red-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105 border border-gray-800">
-                <a href="mailto:ceo@shoolin.co.uk" target="_blank"><IoMailOutline size={24} className="text-white" /></a>
+            <li className="w-fit bg-[#0077b5] hover:bg-blue-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105 border border-gray-800">
+                <a href="https://www.linkedin.com/in/sargam-pandey-74bb9a6b/" target="_blank"><FaLinkedin size={24} className="text-white" /></a>
             </li>
-            <li className="w-fit bg-blue-600 hover:bg-blue-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105 border border-gray-800">
-                <a href="tel:+918569856987" target="_blank"><IoMdCall size={24} className="text-white" /></a>
+            <li className="w-fit bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105 border border-gray-800">
+                <a href="https://instagram.com/sargampandey_" target="_blank"><FaInstagram size={24} className="text-white" /></a>
             </li>
             <li className="w-fit bg-green-500 hover:bg-green-600 rounded-full p-1.5 transition-all duration-300 hover:scale-105 border border-gray-800">
                 <a href="https://wa.me/9100000000" target="_blank"><FaWhatsapp size={24} className="text-white" /></a>
             </li>
-            <li className="w-fit bg-gradient-to-r from-purple-600 to-pink-600 hover:from-purple-700 hover:to-pink-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105 border border-gray-800">
-                <a href="https://instagram.com/sargampandey_" target="_blank"><FaInstagram size={24} className="text-white" /></a>
+            <li className="w-fit bg-blue-600 hover:bg-blue-700 rounded-full p-1.5 transition-all duration-300 hover:scale-105 border border-gray-800">
+                <a href="tel:+918569856987" target="_blank"><IoMdCall size={24} className="text-white" /></a>
             </li>
         </ul>
     )
